c22eb72a3b78210dc683462b3a08e77c9055ca75
Aixia SONG <iveysong@gmail.com>
2023-11-20 12:48:42 +0800

    BatchScript

---
+++ Preprocessing/BatchScripting/forsinglesubject/Batch_Script.m
@@ -0,0 +1,201 @@
+% List of open inputs
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+% fMRI model specification: Onsets - cfg_entry
+nrun = 12; % enter the number of runs here
+jobfile = {'E:\fMRI\ds000105_R2.0.2_raw\Batch_Script_job.m'};
+jobs = repmat(jobfile, 1, nrun);
+inputs = cell(96, nrun);
+for crun = 1:nrun
+    inputs{1, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{2, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{3, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{4, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{5, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{6, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{7, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{8, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{9, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{10, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{11, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{12, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{13, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{14, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{15, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{16, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{17, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{18, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{19, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{20, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{21, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{22, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{23, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{24, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{25, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{26, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{27, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{28, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{29, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{30, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{31, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{32, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{33, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{34, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{35, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{36, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{37, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{38, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{39, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{40, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{41, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{42, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{43, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{44, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{45, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{46, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{47, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{48, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{49, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{50, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{51, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{52, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{53, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{54, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{55, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{56, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{57, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{58, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{59, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{60, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{61, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{62, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{63, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{64, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{65, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{66, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{67, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{68, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{69, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{70, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{71, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{72, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{73, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{74, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{75, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{76, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{77, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{78, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{79, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{80, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{81, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{82, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{83, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{84, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{85, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{86, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{87, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{88, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{89, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{90, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{91, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{92, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{93, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{94, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{95, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+    inputs{96, crun} = MATLAB_CODE_TO_FILL_INPUT; % fMRI model specification: Onsets - cfg_entry
+end
+spm('defaults', 'FMRI');
+spm_jobman('run', jobs, inputs{:});
